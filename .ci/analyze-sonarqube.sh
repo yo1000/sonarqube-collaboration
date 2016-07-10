722e254a5dfcb6c5bcd515e759aae305751ee3e8
Yoichi KIKUCHI <yoichi.kikuchi.aug@gmail.com>
2016-07-10 23:23:01 +0900

    Update analyze-sonarqube.sh

--- .ci/analyze-sonarqube.sh
+++ .ci/analyze-sonarqube.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-PR_NUMBER=`echo "${CI_PULL_REQUEST}" | sed -e 's/.*\///g'`
-
 echo "${CI_PULL_REQUEST}"
+
+PR_NUMBER=`echo "${CI_PULL_REQUEST}" | sed -e 's/.*\/pull\///g'`
+echo "${PR_NUMBER}"
+
+if [ -z "${PR_NUMBER}" ]; then
+  exit 0
+fi
+
+PR_NUMBER=`echo "${CI_PULL_REQUEST}" | sed -e 's/.*\///g'`
 echo "${PR_NUMBER}"
 
 echo "mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.0.1:sonar"
@@ -18,17 +25,15 @@ echo "  -Dsonar.github.oauth=****"
 echo "  -Dsonar.github.repository=${GITHUB_REPOS}"
 echo "  -B -e -V"
 
-if [ -n "${PR_NUMBER}" ]; then
-  mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.0.1:sonar \
-    -Dmaven.test.skip=true			\
-    -Dclirr=true				\
-    -Dsonar.host.url="${SONARQUBE_URL}"		\
-    -Dsonar.login="${SONARQUBE_LOGIN}"		\
-    -Dsonar.password="${SONARQUBE_PASSWORD}"	\
-    -Dsonar.sourceEncoding=UTF-8		\
-    -Dsonar.analysis.mode=issues		\
-    -Dsonar.github.pullRequest="${PR_NUMBER}"	\
-    -Dsonar.github.oauth="${GITHUB_OAUTH}"	\
-    -Dsonar.github.repository="${GITHUB_REPOS}"	\
-    -B -e -V
-fi
+mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.0.1:sonar \
+  -Dmaven.test.skip=true			\
+  -Dclirr=true				\
+  -Dsonar.host.url="${SONARQUBE_URL}"		\
+  -Dsonar.login="${SONARQUBE_LOGIN}"		\
+  -Dsonar.password="${SONARQUBE_PASSWORD}"	\
+  -Dsonar.sourceEncoding=UTF-8		\
+  -Dsonar.analysis.mode=issues		\
+  -Dsonar.github.pullRequest="${PR_NUMBER}"	\
+  -Dsonar.github.oauth="${GITHUB_OAUTH}"	\
+  -Dsonar.github.repository="${GITHUB_REPOS}"	\
+  -B -e -V
